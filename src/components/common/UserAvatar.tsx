be55150b0ffbb8f277b3b7481d3b4ead5dc7fe5f
Yauhen <yau.vink@gmail.com>
2025-08-24 01:33:22 +0200

    add

--- src/components/common/UserAvatar.tsx
+++ src/components/common/UserAvatar.tsx
@@ -1,12 +1,35 @@
-import { Box, Avatar } from '@mui/material';
+import { Box, Avatar, Typography } from '@mui/material';
 import { Person } from '@mui/icons-material';
 import type { GameUser } from '../../providers/GameProvider/GameProvider';
 
 function UserAvatar({ user }: { user: GameUser }) {
-  const size = 100;
+  const size = 70;
 
   return (
-    <Box sx={{ width: `${size}px`, minHeight: `${size}px`, display: 'flex', flexDirection: 'column' }}>
+    <Box
+      sx={{
+        // border: '1px solid red',
+        width: `${size}px`,
+        minHeight: `${size}px`,
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Typography
+        sx={{
+          // border: '1px solid red',
+          zIndex: 9,
+          position: 'absolute',
+          bottom: '5px',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          color: 'white',
+          textShadow: '1px 1px 2px black, 0 0 1em black, 0 0 0.2em black',
+        }}
+      >
+        {user.first_name}
+      </Typography>
+
       {user?.photo_url ? (
         <Avatar
           src={user.photo_url}
@@ -20,6 +43,17 @@ function UserAvatar({ user }: { user: GameUser }) {
       ) : (
         <Person sx={{ width: size, height: size }} />
       )}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '0px',
+          right: '0px',
+          backgroundColor: user.is_online ? 'green' : 'grey',
+          borderRadius: '50%',
+          width: '20px',
+          height: '20px',
+        }}
+      ></Box>
     </Box>
   );
 }
